5c7a2923c6003ba35d8c570a094355bd27dc616b
sanskarchoudhry <sanskar22101@gmail.com>
2025-02-07 22:32:52 +0530

    feat: addded boilerplate for dynamic form

---
+++ medium-practice-next/src/app/dynamic-form-builder/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function FormPage() {
+  return <div>FormPage</div>;
+}
+
+// TODO: Make a Data Object to keep how many inputs/form elements are there
+// TODO: Make a Dropdown to add new elements.
+// TODO: User selects which element to add using the Popup
